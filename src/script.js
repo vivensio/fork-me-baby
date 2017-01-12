ceda8a6343eaedee6b67e69aa405204548a6960f
Vivek Patel <vvivek.patel@gmail.com>
2017-01-12 16:32:32 +0100

    namespace

--- src/script.js
+++ src/script.js
@@ -1,26 +1,29 @@
-function appendMarkup(pos, link, sticky) {
-    var markup = 'SRC_MARKUP';
-    document.body.insertAdjacentHTML('beforeend', markup);
-    var forkMeBaby = document.querySelector('.fork-me-baby');
+(function () {
+    var forkMeBaby = function (options) {
+        var pos = options.position || 'top-right';
+        var link = options.link;
+        var sticky = options.sticky || false;
 
-    forkMeBaby.classList.add(pos);
-    forkMeBaby.querySelector('a').setAttribute('href', link);
-    if (sticky) forkMeBaby.classList.add('fork-me-baby--sticky');
+        if (!link) console.warn('No github link!');
+        appendMarkup(pos, link, sticky);
+    };
+    var appendMarkup = function (pos, link, sticky) {
+        var markup = 'SRC_MARKUP';
+        document.body.insertAdjacentHTML('beforeend', markup);
+        var forkMeBaby = document.querySelector('.fork-me-baby');
+
+        forkMeBaby.classList.add(pos);
+        forkMeBaby.querySelector('a').setAttribute('href', link);
+        if (sticky) forkMeBaby.classList.add('fork-me-baby--sticky');
+        
+        appendStyles();
+    };
+    var appendStyles = function () {
+        var style = document.createElement('style');
+        style.innerHTML = 'SRC_STYLE';
+        document.querySelector('body').appendChild(style);
+    }
+
+    window.forkMeBaby = forkMeBaby;
     
-    appendStyles();
-}
-
-function appendStyles() {
-    var style = document.createElement('style');
-    style.innerHTML = 'SRC_STYLE';
-    document.querySelector('body').appendChild(style);
-}
-
-function forkMeBaby(data) {
-    var pos = data.position || 'top-right';
-    var link = data.link;
-    var sticky = data.sticky || false;
-
-    if (!link) console.warn('No github link!');
-    appendMarkup(pos, link, sticky);
-}
+})();
